f69e7ea15047f60dbb424d068a085ef9475df9b4
GisKook <zhangkai.gis@163.com>
2016-02-08 19:32:45 +0800

    add new version jar

--- app/src/main/java/com/example/zhangkai/gpstraker/AlarmReceiver.java
+++ app/src/main/java/com/example/zhangkai/gpstraker/AlarmReceiver.java
@@ -6,8 +6,9 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Environment;
-
 import com.commonsware.cwac.locpoll.LocationPoller;
+import com.commonsware.cwac.locpoll.LocationPollerParameter;
+import com.commonsware.cwac.locpoll.LocationPollerResult;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -19,17 +20,9 @@ import java.util.Date;
  * Created by zhangkai on 2016/2/4.
  */
 public class AlarmReceiver extends BroadcastReceiver {
-    public static final int REQUEST_CODE = 12345;
-
     @Override
     public void onReceive(Context context, Intent intent) {
-//        Intent i = new Intent(context, ReportLocationDataService.class);
-//        i.putExtra("hello","world");
-//        context.startService(i);
-
-        File log =
-                new File(Environment.getExternalStorageDirectory(),
-                        "LocationLog.txt");
+        File log = new File(Environment.getExternalStorageDirectory(),"LocationLog.txt");
 
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter(log.getAbsolutePath(), log.exists()));
@@ -38,28 +31,34 @@ public class AlarmReceiver extends BroadcastReceiver {
             out.write(" : ");
 
             Bundle b = intent.getExtras();
-            Location loc = (Location) b.get(LocationPoller.EXTRA_LOCATION);
+
+            LocationPollerResult locationResult = new LocationPollerResult(b);
+
+            Location loc=locationResult.getLocation();
             String msg;
 
-            if (loc == null) {
-                loc = (Location) b.get(LocationPoller.EXTRA_LASTKNOWN);
+            if (loc==null) {
+                loc=locationResult.getLastKnownLocation();
 
-                if (loc == null) {
-                    msg = intent.getStringExtra(LocationPoller.EXTRA_ERROR);
-                } else {
-                    msg = "TIMEOUT, lastKnown=" + loc.toString();
+                if (loc==null) {
+                    msg=locationResult.getError();
                 }
-            } else {
-                msg = loc.toString();
+                else {
+                    msg="TIMEOUT, lastKnown="+loc.toString();
+                }
+            }
+            else {
+                msg=loc.toString();
             }
 
-            if (msg == null) {
-                msg = "Invalid broadcast received!";
+            if (msg==null) {
+                msg="Invalid broadcast received!";
             }
 
             out.write(msg);
             out.write("\n");
             out.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/example/zhangkai/gpstraker/GPSActivity.java
+++ app/src/main/java/com/example/zhangkai/gpstraker/GPSActivity.java
@@ -5,20 +5,18 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.SystemClock;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+
 import com.commonsware.cwac.locpoll.LocationPoller;
+import com.commonsware.cwac.locpoll.LocationPollerParameter;
 
 
 public class GPSActivity extends AppCompatActivity {
 
-    private static final int PERIOD=2000*90;  // 30 minutes
+    private static final int PERIOD=1000*30;  // 30 minutes
     private PendingIntent pi=null;
     private AlarmManager mgr=null;
 
@@ -35,37 +33,18 @@ public class GPSActivity extends AppCompatActivity {
 
         Intent i=new Intent(this, LocationPoller.class);
 
-        i.putExtra(LocationPoller.EXTRA_INTENT,
-                new Intent(this, AlarmReceiver.class));
-//        i.putExtra(LocationPoller.EXTRA_PROVIDER,
-//                LocationManager.NETWORK_PROVIDER);
-        i.putExtra(LocationPoller.EXTRA_PROVIDER,
-                LocationManager.GPS_PROVIDER);
+        Bundle bundle = new Bundle();
+        LocationPollerParameter parameter = new LocationPollerParameter(bundle);
+        parameter.setIntentToBroadcastOnCompletion(new Intent(this, AlarmReceiver.class));
+       // try GPS and fall back to NETWORK_PROVIDER
+        parameter.setProviders(new String[] {LocationManager.GPS_PROVIDER, LocationManager.NETWORK_PROVIDER});
+        parameter.setTimeout(6000);
+        i.putExtras(bundle);
 
         pi=PendingIntent.getBroadcast(this, 0, i, 0);
         mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                 SystemClock.elapsedRealtime(),
                 PERIOD,
                 pi);
-//        // Construct an intent that will execute the AlarmReceiver
-//        Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
-//        // Create a PendingIntent to be triggered when the alarm goes off
-//        final PendingIntent pIntent = PendingIntent.getBroadcast(this, AlarmReceiver.REQUEST_CODE,
-//                intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        // Setup periodic alarm every 5 seconds
-//        long firstMillis = System.currentTimeMillis(); // alarm is set right away
-//        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-//        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
-//        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
-//        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
-//                30*AlarmManager.INTERVAL_HALF_HOUR/1800, pIntent);
-    }
-
-    public void cancelAlarm() {
-        Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
-        final PendingIntent pIntent = PendingIntent.getBroadcast(this, AlarmReceiver.REQUEST_CODE,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        alarm.cancel(pIntent);
     }
 }
