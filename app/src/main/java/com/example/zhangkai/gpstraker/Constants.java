31e75b6395c11983f027981beefe1690621e7ffd
GisKook <zhangkai.gis@163.com>
2016-02-24 21:38:54 +0800

    the mqtt's timeout can not set to big.If so the app will crash.It seems the same with the location timeout,now from poor network conditions to good it can report t he data.todo test reconnect in poor network conditions

--- app/src/main/java/com/example/zhangkai/gpstraker/Constants.java
+++ app/src/main/java/com/example/zhangkai/gpstraker/Constants.java
@@ -13,9 +13,9 @@ public class Constants {
     public static final String MQTTTOPIC="zhangkai";
     public static final String MQTTBROKERHOST="test.mosquitto.org";
     public static final int MQTTBROKERPORT=1883;
-    public static final int MQTTKEEPALIVEINTERVAL=60;
-    public static final int MQTTCONNTIMEOUT=5;
-    public static final int MQTTMAXCONNTIMEOUT=120;
+    public static final int MQTTKEEPALIVEINTERVAL=30;
+    public static final int MQTTCONNTIMEOUT=20;
+    public static final int MQTTMAXCONNTIMEOUT=20;
 
     public static final int LOCATIONPERIOD=30*1000;
     public static final int LOCATIONGPSTIMEOUT=8*1000;
